a9fc086b330650eb72a77bdd5055cf4cf4f2b863
andyreesecups <andyreesecups@yahoo.com>
2017-06-23 10:47:19 -0500

    prettified code and fixed bug

--- javascript/logic.js
+++ javascript/logic.js
@@ -1,98 +1,98 @@
 var player = 1;
 
 function clickBtn(btn) {
-	if(player == 1) {
-document.getElementById(btn).value="X";
-document.getElementById(btn).disabled="disabled";
-player -= 1;
-winner();
-}else{
-document.getElementById(btn).value="O";	
-document.getElementById(btn).disabled="disabled";
-player += 1;
-winner();
-}
+    if (player == 1) {
+        document.getElementById(btn).value = "X";
+        document.getElementById(btn).disabled = "disabled";
+        player -= 1;
+        winner();
+    } else {
+        document.getElementById(btn).value = "O";
+        document.getElementById(btn).disabled = "disabled";
+        player += 1;
+        winner();
+    }
 }
 // if player has 3 X's in a row vertically, diagonally, and horizantally then alert X wins //
 function winner() {
-	if(document.getElementById("btn1").value == "X" &&
-		document.getElementById("btn2").value == "X" &&
-		document.getElementById("btn3").value == "X" || 
-		document.getElementById("btn4").value == "X" &&
-		document.getElementById("btn5").value == "X" &&
-		document.getElementById("btn6").value == "X" ||
-		document.getElementById("btn7").value == "X" &&
-		document.getElementById("btn8").value == "X" &&
-		document.getElementById("btn9").value == "X" ||
-		document.getElementById("btn1").value == "X" &&
-		document.getElementById("btn4").value == "X" &&
-		document.getElementById("btn7").value == "X" || 
-		document.getElementById("btn2").value == "X" &&
-		document.getElementById("btn5").value == "X" &&
-		document.getElementById("btn8").value == "X" ||
-		document.getElementById("btn3").value == "X" &&
-		document.getElementById("btn6").value == "X" &&
-		document.getElementById("btn9").value == "X" || 
-		document.getElementById("btn1").value == "X" &&
-		document.getElementById("btn5").value == "X" &&
-		document.getElementById("btn9").value == "X" ||
-		document.getElementById("btn3").value == "X" &&
-		document.getElementById("btn5").value == "X" &&
-		document.getElementById("btn7").value == "X" 
+    if (document.getElementById("btn1").value == "X" &&
+        document.getElementById("btn2").value == "X" &&
+        document.getElementById("btn3").value == "X" ||
+        document.getElementById("btn4").value == "X" &&
+        document.getElementById("btn5").value == "X" &&
+        document.getElementById("btn6").value == "X" ||
+        document.getElementById("btn7").value == "X" &&
+        document.getElementById("btn8").value == "X" &&
+        document.getElementById("btn9").value == "X" ||
+        document.getElementById("btn1").value == "X" &&
+        document.getElementById("btn4").value == "X" &&
+        document.getElementById("btn7").value == "X" ||
+        document.getElementById("btn2").value == "X" &&
+        document.getElementById("btn5").value == "X" &&
+        document.getElementById("btn8").value == "X" ||
+        document.getElementById("btn3").value == "X" &&
+        document.getElementById("btn6").value == "X" &&
+        document.getElementById("btn9").value == "X" ||
+        document.getElementById("btn1").value == "X" &&
+        document.getElementById("btn5").value == "X" &&
+        document.getElementById("btn9").value == "X" ||
+        document.getElementById("btn3").value == "X" &&
+        document.getElementById("btn5").value == "X" &&
+        document.getElementById("btn7").value == "X"
 
-		) {
-alert("The winner is X");
-reset();
-// if player has 3 O's in a row vertically, diagonally, and horizantally then alert O wins //
-} else 
-		if(document.getElementById("btn1").value == "O" &&
-		document.getElementById("btn2").value == "O" &&
-		document.getElementById("btn3").value == "O" || 
-		document.getElementById("btn4").value == "O" &&
-		document.getElementById("btn5").value == "O" &&
-		document.getElementById("btn6").value == "O" ||
-		document.getElementById("btn7").value == "O" &&
-		document.getElementById("btn8").value == "O" &&
-		document.getElementById("btn9").value == "O" ||
-		document.getElementById("btn1").value == "O" &&
-		document.getElementById("btn4").value == "O" &&
-		document.getElementById("btn7").value == "O" || 
-		document.getElementById("btn2").value == "O" &&
-		document.getElementById("btn5").value == "O" &&
-		document.getElementById("btn8").value == "O" ||
-		document.getElementById("btn3").value == "O" &&
-		document.getElementById("btn6").value == "O" &&
-		document.getElementById("btn9").value == "O" || 
-		document.getElementById("btn1").value == "O" &&
-		document.getElementById("btn5").value == "O" &&
-		document.getElementById("btn9").value == "O" ||
-		document.getElementById("btn3").value == "O" &&
-		document.getElementById("btn5").value == "O" &&
-		document.getElementById("btn7").value == "O" 
+    ) {
+        alert("The winner is X");
+        reset();
+        // if player has 3 O's in a row vertically, diagonally, and horizantally then alert O wins //
+    } else
+    if (document.getElementById("btn1").value == "O" &&
+        document.getElementById("btn2").value == "O" &&
+        document.getElementById("btn3").value == "O" ||
+        document.getElementById("btn4").value == "O" &&
+        document.getElementById("btn5").value == "O" &&
+        document.getElementById("btn6").value == "O" ||
+        document.getElementById("btn7").value == "O" &&
+        document.getElementById("btn8").value == "O" &&
+        document.getElementById("btn9").value == "O" ||
+        document.getElementById("btn1").value == "O" &&
+        document.getElementById("btn4").value == "O" &&
+        document.getElementById("btn7").value == "O" ||
+        document.getElementById("btn2").value == "O" &&
+        document.getElementById("btn5").value == "O" &&
+        document.getElementById("btn8").value == "O" ||
+        document.getElementById("btn3").value == "O" &&
+        document.getElementById("btn6").value == "O" &&
+        document.getElementById("btn9").value == "O" ||
+        document.getElementById("btn1").value == "O" &&
+        document.getElementById("btn5").value == "O" &&
+        document.getElementById("btn9").value == "O" ||
+        document.getElementById("btn3").value == "O" &&
+        document.getElementById("btn5").value == "O" &&
+        document.getElementById("btn7").value == "O"
 
-		) {
-alert("The winner is O");
-reset();
-}
+    ) {
+        alert("The winner is O");
+        reset();
+    }
 }
 
 function reset() {
-		document.getElementById("btn1").value = "";
-		document.getElementById("btn2").value = "";
-		document.getElementById("btn3").value = "";
-		document.getElementById("btn4").value = "";
-		document.getElementById("btn5").value = "";
-		document.getElementById("btn6").value = "";
-		document.getElementById("btn7").value = "";
-		document.getElementById("btn8").value = "";
-		document.getElementById("btn9").value = "";
-		document.getElementById("btn1").disabled = "";
-		document.getElementById("btn2").disabled = "";
-		document.getElementById("btn3").disabled = "";
-		document.getElementById("btn4").disabled = "";
-		document.getElementById("btn5").disabled = "";
-		document.getElementById("btn6").disabled = "";
-		document.getElementById("btn7").disabled = "";
-		document.getElementById("btn8").disabled = "";
-		document.getElementById("btn9").disabled = "";
+    document.getElementById("btn1").value = "";
+    document.getElementById("btn2").value = "";
+    document.getElementById("btn3").value = "";
+    document.getElementById("btn4").value = "";
+    document.getElementById("btn5").value = "";
+    document.getElementById("btn6").value = "";
+    document.getElementById("btn7").value = "";
+    document.getElementById("btn8").value = "";
+    document.getElementById("btn9").value = "";
+    document.getElementById("btn1").disabled = "";
+    document.getElementById("btn2").disabled = "";
+    document.getElementById("btn3").disabled = "";
+    document.getElementById("btn4").disabled = "";
+    document.getElementById("btn5").disabled = "";
+    document.getElementById("btn6").disabled = "";
+    document.getElementById("btn7").disabled = "";
+    document.getElementById("btn8").disabled = "";
+    document.getElementById("btn9").disabled = "";
 }
